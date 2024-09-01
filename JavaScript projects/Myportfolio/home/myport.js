1a439e97dee32727b48db26b67456e87a5e760c8
Maro-code <timilehin332@gmail.com>
2024-09-01 22:54:25 +0100

    initial commit

--- JavaScript projects/Myportfolio/home/myport.js
+++ JavaScript projects/Myportfolio/home/myport.js
@@ -1,5 +1,13 @@
 document.addEventListener('DOMContentLoaded', function() {
     let button = document.getElementById("btnprimary");
+    let targ = document.getElementById("targ");
+    targ.style.display = "none";
+    button.addEventListener("mouseover",() =>{
+        targ.style.display = "flex";
+    })
+    button.addEventListener("mouseout",() =>{
+        targ.style.display = "none";
+    })
     button.addEventListener("click",() =>{
         window.open("https://maro-code.github.io/SIWES-projects/JavaScript%20projects/Myportfolio/resume/resume.html");
     })
